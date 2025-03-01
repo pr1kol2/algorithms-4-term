156702c00c4432cf0365e06c0cd25e8d3d94cb7f
cyril grinko <k.a.grinko@gmail.com>
2025-03-01 17:25:17 +0300

    A solved

--- 1st_contest/A.cpp
+++ 1st_contest/A.cpp
@@ -1,10 +1,47 @@
+#include <algorithm>
 #include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<int> GetZFunction(std::string s) {
+  int n = s.size();
+  std::vector<int> z(n, 0);
+  int left = 0;
+  int right = 0;
+  for (int i = 1; i < n; ++i) {
+    if (i <= right) {
+      z[i] = std::min(z[i - left], right - i + 1);
+    }
+    while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+      ++z[i];
+    }
+    if (i + z[i] - 1 > right) {
+      left = i;
+      right = i + z[i] - 1;
+    }
+  }
+  return z;
+}
+
+void PrintPatternOccurrencesInString(const std::string& s,
+                                     const std::string& p) {
+  auto z = GetZFunction(p + '#' + s);
+
+  for (int i = 0; i < z.size(); ++i) {
+    if (z[i] == p.size()) {
+      std::cout << i - (1 + p.size()) << '\n';
+    }
+  }
+}
 
 int main() {
-  int n;
-  std::cin >> n;
-  for (int i = 1; i <= 2 * n; i++) {
-    std::cout << i << " ";
-  }
+  std::string s;
+  std::string p;
+
+  std::cin >> s;
+  std::cin >> p;
+
+  PrintPatternOccurrencesInString(s, p);
+
   return 0;
 }

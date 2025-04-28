27153591a518f01a9d5fcca939c973d7d79e1309
cyril grinko <k.a.grinko@gmail.com>
2025-04-28 18:26:37 +0300

    minor fixes

--- 2nd_contest/B.cpp
+++ 2nd_contest/B.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 #include <iostream>
 #include <vector>
 
@@ -5,20 +6,20 @@ int64_t CalculateLeastPrimeDivisorSum(int n) {
   std::vector<int> least_prime_divisor(n + 1, 0);
   std::vector<int> primes;
 
-  for (int i = 2; i <= n; ++i) {
+  for (uint32_t i = 2; i <= n; ++i) {
     if (least_prime_divisor[i] == 0) {
       least_prime_divisor[i] = i;
       primes.push_back(i);
     }
   
-    for (int p : primes) {
+    for (uint32_t p : primes) {
       if (p > least_prime_divisor[i] || p * i > n) break;
       least_prime_divisor[p * i] = p;
     }
   }
 
-  int64_t sum = 0;
-  for (int i = 3; i <= n; ++i) {
+  uint64_t sum = 0;
+  for (uint32_t i = 3; i <= n; ++i) {
     if (i != least_prime_divisor[i]) {
       sum += least_prime_divisor[i];
     }
@@ -27,10 +28,10 @@ int64_t CalculateLeastPrimeDivisorSum(int n) {
 }
 
 int main() {
-  int n = 0;
+  uint32_t n = 0;
   std::cin >> n;
 
-  std::cout << CalculateLeastPrimeDivisorSum(n) << std::endl;
+  std::cout << CalculateLeastPrimeDivisorSum(n) << '\n';
   
   return 0;
 }
